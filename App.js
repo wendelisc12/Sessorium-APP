f263c8e20d7a867ab21173a3e98ab1569eb078cd
Carlos Eduardo <carlos_eduardo565@hotmail.com>
2022-01-07 23:59:05 -0300

    cadastrarcoordenador

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="registrarescola">
+      <Stack.Navigator initialRouteName="cadastrarcoordenador">
         <Stack.Screen name="login" component={login} options={{headerShown: false}} />
         <Stack.Screen name="loginAdministrador" component={loginAdministrador} options={{ title:""}} />
         <Stack.Screen name="registrarescola" component={registrarescola} options={{ title:""}} />
