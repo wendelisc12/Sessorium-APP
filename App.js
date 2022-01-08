50b4358d89e550e8812479ce9bd45a27c52023bf
gfms18 <business55081@gmail.com>
2022-01-08 00:05:07 -0300

    resolvendo erro"

--- App.js
+++ App.js
@@ -14,18 +14,11 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <Stack.Navigator initialRouteName="login">
-=======
       <Stack.Navigator initialRouteName="cadastrarcoordenador">
->>>>>>> f263c8e20d7a867ab21173a3e98ab1569eb078cd
         <Stack.Screen name="login" component={login} options={{headerShown: false}} />
         <Stack.Screen name="loginAdministrador" component={loginAdministrador} options={{ title:""}} />
         <Stack.Screen name="registrarescola" component={registrarescola} options={{ title:""}} />
         <Stack.Screen name="cadastrarcoordenador" component={cadastrarcoordenador} options={{ title:""}} />
-
-        
-
       </Stack.Navigator>
     </NavigationContainer>
   );
